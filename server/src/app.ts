b6528e442d1a64a09809f6f93f80561b90889fda
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-09-18 22:04:58 +0600

    Server retested

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import http from "http";
 import { Server } from "socket.io";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import { RootRoutes } from "./routes/root.routes";
 dotenv.config();
 
@@ -11,11 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(morgan("dev"));
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://talktive-beryl.vercel.app"],
   },
 });
 
