8f1b8fbc9903bc0dde9ac4777e75a5173c35ff82
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-09-22 01:29:01 +0600

    Cors issue fixed

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,9 +14,14 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(cookieParser());
 app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://talktive-beryl.vercel.app"],
+    credentials: true,
+  })
+);
 
 const server = http.createServer(app);
 const io = new Server(server, {
