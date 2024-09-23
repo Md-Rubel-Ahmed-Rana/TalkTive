fe95f28072d90f440a9a51cf544cff4ec44d85d8
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-09-24 02:16:56 +0600

    Frontend make ready to build and deploy

--- client/src/features/api/apiSlice.tsx
+++ client/src/features/api/apiSlice.tsx
@@ -1,9 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { baseApi } from ".";
 
 const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5050",
+    baseUrl: baseApi,
     credentials: "include",
   }),
   tagTypes: ["user", "message", "chat"] as any,
