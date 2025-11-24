e3e0598c227dfdfaffb420f1313ac46a7310865a
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-11-25 01:20:55 +0600

    login page design fully completed

--- frontend/src/auth/login.tsx
+++ frontend/src/auth/login.tsx
@@ -1,15 +1,19 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import LoginForm from "./login-form";
 
-
- const Login = () => {
+const Login = () => {
   return (
-    <div className="w-screen h-screen relative flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url('/auth-bg-image.png')` }}>
-      <div className="absolute inset-0 bg-black/40 backdrop-blur-sm" />
+    <div
+      className="w-screen h-screen relative flex items-center justify-center bg-cover bg-center bg-repeat-0"
+      style={{ backgroundImage: `url('/auth-bg-image.png')` }}
+    >
+      <div className="absolute inset-0 bg-black/40 backdrop-blur-xs" />
 
       <Card className="w-full max-w-md relative z-10 shadow-2xl border-0 bg-white/90 backdrop-blur-xl rounded-2xl">
         <CardHeader>
-          <CardTitle className="text-center text-2xl font-bold">Welcome Back</CardTitle>
+          <CardTitle className="text-center text-2xl font-bold">
+            Welcome Back
+          </CardTitle>
         </CardHeader>
         <CardContent>
           <LoginForm />
@@ -17,6 +21,6 @@ import LoginForm from "./login-form";
       </Card>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
