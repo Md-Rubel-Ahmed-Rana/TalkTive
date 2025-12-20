04f2daef3c6250bdd87ee245bc3ceb7377c8290f
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-12-21 02:34:13 +0600

    feat(socket): refactor socket initialization and improve type definition

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,10 +1,12 @@
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
-const baseApiSocket = process.env.NEXT_PUBLIC_BASE_API_SOCKET as string;
+const SOCKET_URL = process.env.NEXT_PUBLIC_BASE_API_SOCKET as string;
 
-export const socket = io(baseApiSocket, {
+export const socket: Socket = io(SOCKET_URL, {
+  autoConnect: false,
   transports: ["websocket"],
   withCredentials: true,
+  reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 2000,
 });
