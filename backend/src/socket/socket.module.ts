2dd8cb4f7da2634d3d9900059456a8d2298c96f5
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-12-20 22:45:18 +0600

    feat(socket): add UsersModule import to SocketModule for user management

--- backend/src/socket/socket.module.ts
+++ backend/src/socket/socket.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { SocketService } from "./socket.service";
 import { SocketGateway } from "./socket.gateway";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
+  imports: [UsersModule],
   providers: [SocketGateway, SocketService],
 })
 export class SocketModule {}
